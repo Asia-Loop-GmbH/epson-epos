38a015458f1b18f68990caf14e6f9996e009058f
Nam Truong Le <lenamtruong@gmail.com>
2023-04-12 14:12:26 +0200

    feature: add more features (#8)
    Co-authored-by: Nam Truong Le <nam.truong.le@sap.com>

--- src/epson.ts
+++ src/epson.ts
@@ -11,11 +11,14 @@ export interface EpsonBuilder {
   ALIGN_RIGHT: string;
   ALIGN_LEFT: string;
   CUT_FEED: string;
+  CUT_NO_FEED: string;
+  FEED_CUTTING: string;
   addTextSize(width: number, height: number): void;
   addTextSmooth(smooth: boolean): void;
   addTextFont(font: string): void;
   addTextStyle(reverse?: boolean, ul?: boolean, em?: boolean, color?: boolean): void;
   addFeed(): void;
+  addFeedPosition(pos: string): void;
   addTextAlign(align: string): void;
   addText(text: string): void;
   addCut(type: string): void;
